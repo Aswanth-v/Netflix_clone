45be7730949af2906f422293976a7dd89fd98db2
Aswabnth.v <aswanthv695@gmail.com>
2024-12-15 08:18:24 -0800

    added math.random

--- src/components/Navbar/Banner/Banner.js
+++ src/components/Navbar/Banner/Banner.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react'
 import {API_KEY,imageurl} from '../../../constants/constants'
 import axios from '../../../axios'
 import './Banner.css'
+
 function Banner() {
   const [movie, setMovie] = useState()
  useEffect(() => {
    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-    console.log(response.data.results[7]);
-    setMovie(response.data.results[7])
+    console.log(response.data.results[Math.floor(Math.random() * 20)]);
+    setMovie(response.data.results[Math.floor(Math.random() * 20)])
    })
  }, [])
  
@@ -18,6 +19,7 @@ function Banner() {
     className='banner'>
         <div className='content'>
         <h1 className='title'>{movie ? movie.name || movie.title : "" }</h1>
+  
         <div className="banner-buttons">
             <button className="button">play</button>
             <button className="button">My List</button>
